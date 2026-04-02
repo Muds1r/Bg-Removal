cb286348fa9a0851af7d28f31b61cd2f37726050
Muhammad Mudasir Munir Khan <139485101+Muds1r@users.noreply.github.com>
2026-04-02 23:36:03 +0500

    Added Logo

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,8 +8,8 @@ const Footer = () => {
             <p className='flex-1  border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @Muds1r | All Right Reserved | 2026</p>
             <div className='flex gap-1'>
                 <img width={40} src={assets.facebook_icon} alt="" />
+                <img width={40} src={assets.instagram_icon} alt="" />
                 <img width={40} src={assets.twitter_icon} alt="" />
-                <img width={40} src={assets.google_plus_icon} alt="" />
             </div>
         </div>
     )
